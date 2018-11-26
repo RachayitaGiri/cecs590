80104fecf7751d6e9ddf767eca9f8492f9a3fc5a
ararbi90 <ararbi90@hotmail.com>
2018-11-26 11:52:14 -0800

    Now we have cifar 100

--- Project/trainBAG2.py
+++ Project/trainBAG2.py
@@ -45,21 +45,21 @@ def baseline_model():
 def getAlexNet():
     #1
     model = Sequential()
-    model.add(Conv2D(96, (11, 11), input_shape=input_shape))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #2
-    model.add(Conv2D(256, (5, 5)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #3
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Conv2D(512, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    #4
-    model.add(ZeroPadding2D((1, 1)))
-    model.add(Conv2D(32, (3, 3)))
+    # model.add(Conv2D(96, (11, 11), input_shape=input_shape))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # #2
+    # model.add(Conv2D(256, (5, 5)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # #3
+    # model.add(ZeroPadding2D((1, 1)))
+    # model.add(Conv2D(512, (3, 3)))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # #4
+    # model.add(ZeroPadding2D((1, 1)))
+    model.add(Conv2D(32, (3, 3), input_shape=input_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     #5
